c9733b4be6373baa546cde53d8c1b719613174e1
Elyes Ferjani <officialef@hotmail.com>
2023-01-12 22:29:14 +0100

    project done

--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods/app.py
@@ -12,29 +12,19 @@ def lcm(num1,num2):
            common_multiplications.append(i) 
     return min(common_multiplications)
 
-
-
-# Create a function named `index` which uses template file named `index.html` 
-# send two numbers as template variable to the app.py and assign route of no path ('/') 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html", methods=["GET"])
+    return render_template("index.html")
 
-
-
-
-# calculate sum of them using "lcm" function, then sent the result to the 
-# "result.hmtl" file and assign route of path ('/calc'). 
-# When the user comes directly "/calc" path, "Since this is a GET request, LCM has not been calculated" string returns to them with "result.html" file
-@app.route("/calc", methods=["GET", "POST"])
-def calculate():
+@app.route('/calc', methods= ["POST"])
+def calc():
     if request.method == "POST":
         num1 = request.form.get("number1")
         num2 = request.form.get("number2")
-        return render_template("result.html", result1 = num1, result2 = num2, lcm = lcm(int(num1),int(num2)), developer_name = 'Oktay')
+        lowest = lcm(int(num1), int(num2))
+        return render_template("result.html", lcm=lowest, result1= num1, result2= num2, developer_name= "Elyes")
     else:
-        return render_template("result.html", developer_name = "Oktay")
+        return "<h1> Expected a HTTP Post Request <h1>"
 
-# Add a statement to run the Flask application which can be debugged.
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(port=3000, debug=True)
